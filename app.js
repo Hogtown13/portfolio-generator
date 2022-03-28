56f61f60be84e98ad0a3a3bea3df938b35710ac7
Hogtown13 <tribenation13@yahoo.com>
2022-03-27 20:14:01 -0400

    added validations

--- app.js
+++ app.js
@@ -1,32 +1,139 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+const promptUser = () => {
+  return inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your Github username!')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const promptProject = portfolioData => {
+  portfolioData.projects = [];
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project? (Required)',
+      validate: projectName => {
+        if (projectName) {
+          return true;
+        } else {
+          console.log ('What is the name of your project!')
+          return false;
+        }
+      }
+    }, 
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)',
+      validate: projectDescription => {
+        if (projectDescription) {
+          return true;
+        } else {
+          console.log ('Provide a description of the project!')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log ('Enter the Github link to your project!')
+          return false;
+        } 
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+};
 
-const [name, github] = profileDataArgs;
+  promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
+  
+
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
+
+//const pageHTML = generatePage(name, github);
 
 
 
+//fs.writeFile('./index.html', pageHTML, err => {
+// if (err) throw err;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-
-  console.log('portfolio complete! Check out index.html to see the output!');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+//  console.log('portfolio complete! Check out index.html to see the output!');
+//});
 
 
 
@@ -43,6 +150,19 @@ fs.writeFile('index.html', generatePage(name, github), err => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// This is just a reference code *IGNORE*
 
 /*const printProfileData = profileDataArr => {
   // This...
